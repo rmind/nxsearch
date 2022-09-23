154922f4b86efd0557d3b1cd242cbe3a540bc586
Mindaugas Rasiukevicius <rmind@noxt.eu>
2022-09-23 23:55:00 +0100

    WIP: Add a Lua test.

---
+++ src/tests/test.lua
@@ -0,0 +1,43 @@
+local nxs = require("nxsearch")
+
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+
+local params = nxs.newparams()
+assert(params)
+
+local index, err = nxs.create("test-index", params)
+if not index then
+  index, err = nxs.open("test-index")
+  assert(index)
+end
+
+index:add(1, "The quick brown fox jumped over the lazy dog")
+index:add(2, "Once upon a time there were three little foxes")
+
+local resp, err = index:search("fox")
+assert(resp)
+
+local results_json = resp:tojson()
+--assert(result_json ==
+--  '{"results":[{"doc_id":2,"score":0.06675427407026291},' ..
+--  '{"doc_id":1,"score":0.06675427407026291}],"count":2}')
+
+--results_table = resp:repr()
+--assert(#result_table == 2)
+
+--assert(result_table[1] == 0.066754274070263)
+--assert(result_table[2] == 0.066754274070263)
+
+print("OK")
