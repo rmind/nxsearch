18f90fc1effe85d80ec3204530709aa336c63ab4
Tolo Palmer <toloco@gmail.com>
2022-09-24 14:29:35 +0100

    Remove diacritics and convert to asciii

--- src/utils/utf8.c
+++ src/utils/utf8.c
@@ -17,15 +17,24 @@
 #include <unicode/utypes.h>
 #include <unicode/ucasemap.h>
 #include <unicode/unorm2.h>
+#include <unicode/utrans.h>
 
 #include "strbuf.h"
 #include "utf8.h"
 #include "utils.h"
 
+/*
+* UCI Rule for transliteration.
+* see more: https://unicode-org.github.io/icu/userguide/transforms/general/
+*/
+static const char NFKD_RULE[] = "NFKD; [:Nonspacing Mark:] Remove; Latin-ASCII; NFKC";
+
+
 struct utf8_ctx {
 	char *			locale;
 	UCaseMap *		csm;
 	const UNormalizer2 *	normalizer;
+	UTransliterator *	transliterator;
 };
 
 /*
@@ -38,6 +47,7 @@ utf8_ctx_create(const char *locale)
 {
 	utf8_ctx_t *ctx;
 	UErrorCode ec;
+	UChar *translit_id;
 
 	ctx = calloc(1, sizeof(utf8_ctx_t));
 	if (!ctx) {
@@ -60,6 +70,22 @@ utf8_ctx_create(const char *locale)
 		return NULL;
 	}
 
+	ec = U_ZERO_ERROR;
+	translit_id = malloc(sizeof(NFKD_RULE) * sizeof(UChar));
+	u_strFromUTF8(translit_id, sizeof(NFKD_RULE), NULL, NFKD_RULE, -1, &ec);
+	if (U_FAILURE(ec)) {
+		free(translit_id);
+		utf8_ctx_destroy(ctx);
+		return NULL;
+	}
+	ec = U_ZERO_ERROR;
+	ctx->transliterator = utrans_openU(translit_id, -1, UTRANS_FORWARD, NULL, 0, NULL, &ec);
+	free(translit_id);
+	if (U_FAILURE(ec)) {
+		utf8_ctx_destroy(ctx);
+		return NULL;
+	}
+
 	return ctx;
 }
 
@@ -68,6 +94,9 @@ utf8_ctx_destroy(utf8_ctx_t *ctx)
 {
 	if (ctx->csm) {
 		ucasemap_close(ctx->csm);
+	}
+	if (ctx->transliterator){
+		utrans_close(ctx->transliterator);
 	}
 	free(ctx->locale);
 	free(ctx);
@@ -139,6 +168,62 @@ utf8_toupper(utf8_ctx_t *ctx, const char *s, char *buf, size_t buflen)
 	if (__predict_false(U_FAILURE(ec) || (size_t)len >= buflen)) {
 		return -1;
 	}
+	return len;
+}
+
+
+/*
+ * utf8_subs_diacritics: uses standard ICU transformation to remove 
+ * diacritic characters.
+ * 
+ * See more: https://unicode-org.github.io/icu/userguide/transforms/general/
+ */
+ssize_t
+utf8_subs_diacritics(utf8_ctx_t *ctx, strbuf_t *buf)
+{
+	UErrorCode ec = U_ZERO_ERROR;
+	uint16_t *ubuf = NULL;
+	size_t ulen;
+	int32_t len, max_len;
+
+	/*
+	 * Convert from UTF-8 to UTF-16: the buffer should be twice as
+	 * large, keeping in mind an extra unit for the NUL terminator, also the
+	 * transformation can produce more characters, therefore give
+	 * a large buffer size (XXX: 3x is an arbitrary choice).
+	 */
+	ulen = roundup2((buf->length + 1) * 3, 64);
+	if ((ubuf = malloc(ulen * 2)) == NULL) {
+		goto out;
+	}
+	if ((len = utf8_to_utf16(ctx, buf->value, ubuf, ulen)) == -1) {
+		goto out;
+	}
+	max_len = len;
+
+	utrans_transUChars(
+		ctx->transliterator, ubuf, &len, 
+		ulen, 0, &max_len, &ec
+	);
+
+	if (__predict_false(U_FAILURE(ec))) {
+		const char *errmsg __unused = u_errorName(ec);
+		app_dbgx("utrans_transUChars() failed: %s", errmsg);
+		goto out;
+	}
+
+	if (strbuf_prealloc(buf, max_len) == -1) {
+		goto out;
+	}
+	len = utf8_from_utf16(ctx, ubuf, buf->value, buf->bufsize);
+	if (len == -1) {
+		goto out;
+	}
+	buf->length = len;
+out:
+	/* Always ensure the string is NUL terminated. */
+	buf->value[buf->length] = '\0';
+	free(ubuf);
 	return len;
 }
 
