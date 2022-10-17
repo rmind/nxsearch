874f67450c0c1e6ce19cf23c5418c9db1d5b5302
Tolo Palmer <toloco@gmail.com>
2022-10-17 09:25:50 +0100

    Build and test SVC

--- tools/svc_test.sh
+++ tools/svc_test.sh
@@ -2,12 +2,22 @@
 
 set -eu
 
-curl -XPOST http://127.0.0.1:8000/test-idx
+curl -s -XPOST http://127.0.0.1:8000/test-idx
 
-curl -XPOST --data "cat dog cow" http://127.0.0.1:8000/test-idx/add/1
-curl -XPOST --data "dog cow" http://127.0.0.1:8000/test-idx/add/2
-curl -XPOST --data "cat cat cat" http://127.0.0.1:8000/test-idx/add/3
+curl -s -XPOST --data "cat dog cow" http://127.0.0.1:8000/test-idx/add/1
+curl -s -XPOST --data "dog cow" http://127.0.0.1:8000/test-idx/add/2
+curl -s -XPOST --data "cat cat cat" http://127.0.0.1:8000/test-idx/add/3
 
-curl -s -XPOST --data "cat" http://127.0.0.1:8000/test-idx/search | jq
+result="$(curl -s -XPOST --data "cat" http://127.0.0.1:8000/test-idx/search | \
+    jq '.results[].doc_id' | sed -z 's/\n/,/g;s/,$/\n/' )"
 
 curl -s -XDELETE http://127.0.0.1:8000/test-idx
+
+expected="3,1"
+
+if [ "$result" != "$expected" ]; then
+    echo "Found doc ids: [${result[@]}] expected: [${expected[@]}]"
+    exit 1
+fi
+
+echo "OK"
